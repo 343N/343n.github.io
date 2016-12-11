9b27806b5b9d7f1180d57eda08f5ea18ba781740
343N <p90thepyro@gmail.com>
2016-12-12 05:06:41 +1100

    preperation for js page transitions
    ;d

---
+++ animateclick.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//     $(".contentBox").click(function() {
+//         console.log("CLICKED!");
+//         $('#container').animate({
+//             left: '-100%'
+//         }, 200, 'swing');
+//         $('#title').animate({
+//             left: '-100%'
+//         }, 200, 'swing');
+//     });
+//
+// });
